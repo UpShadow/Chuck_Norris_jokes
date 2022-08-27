d325238b151a0b5dc8f64b3358c851de94f5b8dd
Carlos Eduardo Correia de Souza <CarlosCorreia1002@gmail.com>
2022-08-27 14:18:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 import express from "express";
+import { appendFile } from 'fs';
+import fs from 'fs/promises'
+const chuck = await fs.readFile("./chuck.json");
+let jchuck = JSON.parse(chuck);
 const app = express();
 
-app.get("/informar-nome/:nome",(req,res) => { //http://localhost:8081/informar-nome/carlos
-    res.send(`${req.params.nome} ${req.params.nome }`);
+//function random joke
+function RandomJoke(dados) {
+    let RJ = Math.round(Math.random()*107);
+    return dados.result[RJ].value;
+}
+
+//function filter joke
+function FilterJoke(dados, filter) {
+    let jokes = [];
+    for(let p of dados.result)
+        if(p.value.includes(filter))
+            jokes.push(p.value);
+    return jokes;   
+}
+
+app.get("/Random-Joke", (req,res) => {
+    let joke = RandomJoke(jchuck);
+    res.send(`${joke}`);    
 })
 
-app.get("/informar-nome",(req,res) => { //http://localhost:8081/informar-nome?nome=carlos&sobrenome=eduardo
-    res.send(`${req.query.nome} ${req.query.sobrenome }`);
+app.get("/Filter-Joke/:filter", (req, res) => {
+    let joke = FilterJoke(jchuck, req.params.filter);
+    res.send(`${joke}`);  
+})
+
+app.get("/Filter-Joke", (req,res) => {
+    let joke = FilterJoke(jchuck, req.query.filter);
+    res.send(`${joke}`); 
 })
 
 // executando o servidor
 app.listen(8081, function () {
     console.log("Servidor na porta 8081");
 });
+
+// definindo a rota raiz
+//app.get("/", function (req, res) {
+//        res.sendFile("/Chuck_Norris_jokes/index.html");
+//});
